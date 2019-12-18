ad2a904a13ea92dea01381434dbe0fe809e81c54
Dawson Moore <moore800@purdue.edu>
2019-12-18 09:35:32 -0600

    Changed default logos to proper RRaP logo

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 	
 @app.route('/')
 def mainPage():
-	return render_template("comingSoon.html")
+	return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug = True)
