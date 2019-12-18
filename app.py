d0e8c87c36723e2326d15fc8961137bede289e41
Dawson Moore <moore800@purdue.edu>
2019-12-18 09:39:05 -0600

    Rolled back to coming soon for test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 	
 @app.route('/')
 def mainPage():
-	return render_template("index.html")
+	return render_template("comingSoon.html")
 
 if __name__ == '__main__':
     app.run(debug = True)
